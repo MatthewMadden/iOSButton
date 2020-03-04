c56f21e910d981f03daf4316e1939b6a5e881472
Matt Madden <matt@matt-madden.com>
2020-03-04 07:41:54 -0500

    Initial Commit

---
+++ BasicButton/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BasicButton
+//
+//  Created by Matthew Madden on 3/4/20.
+//  Copyright © 2020 Matthew Madden. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
