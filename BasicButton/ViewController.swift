507b748ca1c4c5280cfaf4b6096e9e4ab61002f0
Matt Madden <matt@matthew-madden.com>
2020-03-04 16:21:52 -0500

    first commit - Button v1.0

--- BasicButton/ViewController.swift
+++ BasicButton/ViewController.swift
@@ -8,13 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+var tapCount = 0;
+var randNum = Int.random(in: 1..<10);
 
+class ViewController: UIViewController {
+    @IBOutlet weak var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onButtonTap(_ sender: Any) {
+        tapCount += 1;
+        
+        if button.backgroundColor == UIColor.red {
+            button.backgroundColor = UIColor.blue
+        } else {
+            button.backgroundColor = UIColor.red
+        }
+        
+        if tapCount >= 20 {
+            button.setTitle(String(randNum), for: .normal)
+        } else if tapCount >= 10 {
+            button.setTitle("Ouch!", for: .normal)
+        }
+        
+        print(tapCount)
+    }
+    
 }
 
